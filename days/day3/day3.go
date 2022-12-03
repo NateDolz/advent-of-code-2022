ed3e8995d55485a5d575b794437d94acbe7e81e5
natedolz <natedolz@gmail.com>
2022-12-03 17:11:35 -0600

    day3 part 2

--- days/day3/day3.go
+++ days/day3/day3.go
@@ -32,6 +32,36 @@ func part1() {
 	fmt.Printf("Total gift value: %d\n", value)
 }
 
+func part2() {
+	fmt.Printf("\n------------ PART 2 ------------\n")
+
+	value := 0
+	idx := 0
+	groups := make([][]rune, 2)
+
+	scanner := util.OpenFile("days/day3/part2.txt")
+
+	for scanner.Scan() {
+		line := []rune(scanner.Text())
+		if idx == 2 {
+			union := util.Union(groups[0], groups[1])
+			union = util.Union(union, line)
+			if len(union) > 1 {
+				fmt.Println(line)
+				log.Fatalln("Too many matches on line")
+			}
+			value += strings.Index(giftValues, string(union[0])) + 1
+			idx = 0
+		} else {
+			groups[idx] = line
+			idx++
+		}
+	}
+
+	fmt.Printf("Total gift value: %d\n", value)
+}
+
 func Run() {
 	part1()
+	part2()
 }
